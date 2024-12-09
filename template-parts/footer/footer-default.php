6dcc42588a66e718493caa428db11bdea7758cbf
TechPort Passport In Evolution <techportgr@gmail.com>
2024-12-09 18:26:01 +0200

    Update footer-default.php

--- template-parts/footer/footer-default.php
+++ template-parts/footer/footer-default.php
@@ -1,1 +1,28 @@
-
+<footer id="colophon" class="site-footer footer-default">
+    <div class="container">
+        <div class="row py-4">
+            <div class="col-md-6">
+                <div class="site-info">
+                    <?php
+                    printf(
+                        esc_html__('© %d %s. All rights reserved.', 'custom-bootstrap-theme'),
+                        date('Y'),
+                        get_bloginfo('name')
+                    );
+                    ?>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <?php
+                wp_nav_menu(array(
+                    'theme_location' => 'footer',
+                    'menu_class'     => 'footer-menu list-inline text-md-end',
+                    'container'      => false,
+                    'fallback_cb'    => false,
+                    'depth'          => 1,
+                ));
+                ?>
+            </div>
+        </div>
+    </div>
+</footer>
