a050eb1f3e045e08dc5757b1fda2208ff3430f41
TechPort Passport In Evolution <techportgr@gmail.com>
2024-12-09 18:29:19 +0200

    Update site-branding.php

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -1,1 +1,26 @@
+<?php
+/**
+ * Site branding template
+ *
+ * @package UrbanCartel
+ */
+?>
+<div class="site-branding">
+    <?php
+    // Custom logo
+    if (has_custom_logo()) {
+        the_custom_logo();
+    } else {
+        // Fallback to site title and description
+        if (is_front_page() && is_home()) : ?>
+            <h1 class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+        <?php else : ?>
+            <p class="site-title"><a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a></p>
+        <?php endif;
 
+        $urbancartel_description = get_bloginfo('description', 'display');
+        if ($urbancartel_description || is_customize_preview()) : ?>
+            <p class="site-description"><?php echo $urbancartel_description; ?></p>
+        <?php endif;
+    } ?>
+</div>
